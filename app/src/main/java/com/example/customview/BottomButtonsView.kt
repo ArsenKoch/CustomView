d113ad6b4fab55737ab44675f6d6ece3072f9971
ArsenKoch <arsenkocaran@icloud.com>
2022-12-02 23:19:48 +0300

    Create and describe all component constructors

--- app/src/main/java/com/example/customview/BottomButtonsView.kt
+++ app/src/main/java/com/example/customview/BottomButtonsView.kt
@@ -1,4 +1,17 @@
 package com.example.customview
 
-class BottomButtonsView {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class BottomButtonsView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    constructor(context: Context,attrs: AttributeSet?,defStyleAttr: Int) : this(context,attrs,defStyleAttr, defStyleRes = 0)
+    constructor(context: Context,attrs: AttributeSet?) : this(context,attrs,0)
+    constructor(context: Context) : this(context,null)
 }
