12f7c33da36de0e26942680ed79487806591073e
ArsenKoch <arsenkocaran@icloud.com>
2022-12-03 00:03:42 +0300

    Handle custom attributes in the component class

--- app/src/main/java/com/example/customview/BottomButtonsView.kt
+++ app/src/main/java/com/example/customview/BottomButtonsView.kt
@@ -1,8 +1,12 @@
 package com.example.customview
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.customview.databinding.PartButtonsBinding
 
 class BottomButtonsView(
     context: Context,
@@ -11,7 +15,59 @@ class BottomButtonsView(
     defStyleRes: Int
 ) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
 
-    constructor(context: Context,attrs: AttributeSet?,defStyleAttr: Int) : this(context,attrs,defStyleAttr, defStyleRes = 0)
-    constructor(context: Context,attrs: AttributeSet?) : this(context,attrs,0)
-    constructor(context: Context) : this(context,null)
+    private val binding: PartButtonsBinding
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(
+        context, attrs, defStyleAttr, defStyleRes = 0
+    )
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        inflater.inflate(R.layout.part_buttons, this, true)
+        binding = PartButtonsBinding.bind(this)
+        initialize(attrs, defStyleAttr, defStyleRes)
+    }
+
+    private fun initialize(attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
+        if (attrs == null) return
+        val typedArray = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.BottomButtonsView,
+            defStyleAttr,
+            defStyleRes
+        )
+
+        val positiveButtonText =
+            typedArray.getString(R.styleable.BottomButtonsView_bottomPositiveButtonText)
+        binding.buttonPositive.text = positiveButtonText ?: "Ok"
+
+        val negativeButtonText =
+            typedArray.getString(R.styleable.BottomButtonsView_bottomNegativeButtonText)
+        binding.buttonNegative.text = negativeButtonText ?: "Cancel"
+
+        val positiveButtonColor = typedArray.getColor(R.styleable.BottomButtonsView_bottomPositiveButtonColor,Color.WHITE)
+        binding.buttonPositive.backgroundTintList = ColorStateList.valueOf(positiveButtonColor)
+
+        val negativeButtonColor = typedArray.getColor(R.styleable.BottomButtonsView_bottomNegativeButtonColor,Color.BLACK)
+        binding.buttonNegative.backgroundTintList = ColorStateList.valueOf(negativeButtonColor)
+
+        val isProgressMode = typedArray.getBoolean(R.styleable.BottomButtonsView_bottomProgressMode,false)
+        if (isProgressMode) {
+            binding.buttonPositive.visibility = INVISIBLE
+            binding.buttonNegative.visibility = INVISIBLE
+            binding.progressBar.visibility = VISIBLE
+        } else {
+            binding.buttonPositive.visibility = VISIBLE
+            binding.buttonNegative.visibility = VISIBLE
+            binding.progressBar.visibility = GONE
+        }
+
+
+
+        typedArray.recycle()
+
+    }
 }
--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -11,7 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(binding.root) }
-
-
     }
 }
