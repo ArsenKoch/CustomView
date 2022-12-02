3a65c71d2233705ddaf8eaba624ac9fd2fce078e
ArsenKoch <arsenkocaran@icloud.com>
2022-12-02 23:02:16 +0300

    Create new class: BottomButtonsView.kt and use tag merge

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -1,11 +1,17 @@
-  package com.example.customview
+package com.example.customview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(binding.root) }
+
+
     }
 }
